ced998aea05e3b7af582ecf90e77d49fdc31f4a9
Sheryf Habib <sheryf.534@gmail.com>
2023-06-18 18:22:30 +0100

    slider-initilization

---
+++ Tracker/src/components/slider.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Slider = () => {
+  return (
+      <View style={styles.coverContainer}>
+          
+    </View>
+  )
+}
+
+export default Slider
+
+const styles = StyleSheet.create({
+    coverContainer: {
+        
+    }
+})
