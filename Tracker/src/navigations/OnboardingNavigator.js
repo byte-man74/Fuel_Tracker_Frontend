24cd480e3469f16c2a65ec0f1984885329d9582b
Sheryf Habib <sheryf.534@gmail.com>
2023-06-18 14:14:37 +0100

    Welcome navigator setup complete 🎉

---
+++ Tracker/src/navigations/OnboardingNavigator.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Welcome from '../screens/OnboardingScreen/welcome';
+
+
+const Stack = createStackNavigator();
+
+function OnboardingNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={Welcome} />
+        </Stack.Navigator>
+    );
+}
+export default OnboardingNavigator
