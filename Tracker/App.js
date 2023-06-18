fbcc4f6e86ba7cb0f088b76a1891910d4e10141d
Sheryf Habib <sheryf.534@gmail.com>
2023-06-18 19:47:24 +0100

    adding customfont

--- Tracker/App.js
+++ Tracker/App.js
@@ -4,6 +4,23 @@ import OnboardingNavigator from './src/navigations/OnboardingNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigations/AuthenticationNavigator';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppRegistry } from 'react-native';
+import { Font } from 'expo-font'; // If using Expo
+
+// Function to load custom fonts
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'CustomFontRegular': require('./assets/fonts/CustomFont-Regular.ttf'),
+    'CustomFontBold': require('./assets/fonts/CustomFont-Bold.ttf'),
+  });
+};
+
+// Call the loadFonts function before rendering the app
+loadFonts().then(() => {
+  // Render the app
+  AppRegistry.registerComponent('Tracker', () => App); // Replace 'MyApp' with your app name
+});
+
 
 
 const Stack = createStackNavigator();
