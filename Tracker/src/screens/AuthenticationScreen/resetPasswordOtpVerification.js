2820a4de84fffeaabde27bf5dd4912b3a946bfa3
Sheryf Habib <sheryf.534@gmail.com>
2023-06-20 14:41:10 +0100

    staging screen for reset password otp page

---
+++ Tracker/src/screens/AuthenticationScreen/resetPasswordOtpVerification.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ResetPasswordOTP = () => {
+  return (
+    <View>
+      <Text>ResetPasswordOTP</Text>
+    </View>
+  )
+}
+
+export default ResetPasswordOTP
+
+const styles = StyleSheet.create({})
