5a6e47139321865237090ab79e8282a4bb31a20e
Sheryf Habib <sheryf.534@gmail.com>
2023-06-20 04:02:48 +0100

    added OTP form

--- Tracker/src/screens/AuthenticationScreen/otpverification.js
+++ Tracker/src/screens/AuthenticationScreen/otpverification.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image, ScrollView, Dimensions, TouchableOpacity, TextInput } from 'react-native';
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import { Ionicons } from 'react-native-vector-icons';
-import Button from '../../components/button';
 
 const { height, width } = Dimensions.get('window');
 
@@ -10,6 +18,21 @@ const SignUp = ({ navigation }) => {
   const handleSignInPress = () => {
     // Navigate to the sign-in page
     navigation.navigate('Login');
+  };
+
+  // Refs for the OTP input fields
+  const otpInputs = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+  ];
+
+  // Function to focus the next input field
+  const focusNextInput = (index) => {
+    if (index < otpInputs.length - 1) {
+      otpInputs[index + 1].current.focus();
+    }
   };
 
   return (
@@ -20,12 +43,26 @@ const SignUp = ({ navigation }) => {
           style={styles.backgroundImage}
         >
           <View style={styles.formHeader}>
-            <Text style={styles.formHeaderTitle}>
-              Enter OTP code
+            <Text style={styles.formHeaderTitle}>Enter OTP code</Text>
+            <Text style={styles.formHeaderText}>
+              Enter the confirmation code sent to your registered email. Kindly go to your email to complete this action.
             </Text>
-              <Text style={styles.formHeaderText}>
-                Enter the confirmation code sent to your registered email. Kindly go to your email to complete this action.
-              </Text>
+            <View style={styles.otpContainer}>
+              {otpInputs.map((input, index) => (
+                <TextInput
+                  key={index}
+                  ref={input}
+                  style={styles.otpInput}
+                  maxLength={1}
+                  keyboardType="numeric"
+                  onChangeText={(text) => {
+                    if (text.length === 1) {
+                      focusNextInput(index);
+                    }
+                  }}
+                />
+              ))}
+            </View>
           </View>
         </ImageBackground>
       </View>
@@ -43,26 +80,43 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'flex-start',
-    minHeight: height * 1.1
+    minHeight: height * 1.1,
   },
   formHeader: {
     width: width,
-    height: 90,
-    justifyContent: 'space-between',
-    marginTop: height * 0.11
+    marginTop: height * 0.11,
+    paddingHorizontal: 30,
   },
   formHeaderTitle: {
     fontFamily: 'MulishBold',
     fontSize: 22,
     color: '#232323',
-    paddingHorizontal: 30
+    marginBottom: 10,
   },
   formHeaderText: {
     fontFamily: 'Regular',
     fontSize: 16,
-    color: '#232323',
-    paddingHorizontal: 30
+    width: '95%',
+    color: '#1E1E1E',
+    marginBottom: 20,
+    lineHeight: 30,
   },
-
+  otpContainer: {
+    flexDirection: 'row',
+    width: '90%', 
+    marginTop: 15,
+    justifyContent: 'space-between',
+  },
+  otpInput: {
+    width: '22%',
+    height: 65,
+    backgroundColor: 'white',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#AAAAAC',
+    borderRadius: 12,
+    paddingHorizontal: 20,
+    fontFamily: 'Regular',
+    fontSize: 16,
+  },
 });
-
