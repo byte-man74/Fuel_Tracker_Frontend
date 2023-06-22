2438fd80584712c610372da24c8341dbfe63a25f
Sheryf Habib <sheryf.534@gmail.com>
2023-06-22 04:23:22 +0100

    adding the map screen to the design

--- Tracker/src/navigations/MainScreen.js
+++ Tracker/src/navigations/MainScreen.js
@@ -3,10 +3,12 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainDashboardNavigator from './MainScreenDashboardNavigation';
 import SavedScreen from '../screens/MainScreen/bookmark/savedScreen';
 import SettingsScreen from '../screens/MainScreen/settings/settings';
+import MapsScreen from '../screens/MainScreen/maps/mapsScreen';
 
 
 const TabArr = [
     { route: 'Dashboard', label: 'Home', activeIcon: require('../icons/home_active.png'), inActiveIcon: require('../icons/home.png'), component: MainDashboardNavigator },
+    { route: 'Maps', label: 'Maps', activeIcon: require('../icons/maps_active.png'), inActiveIcon: require('../icons/maps.png'), component: MapsScreen },
     { route: 'Saved', label: 'Saved', activeIcon: require('../icons/bookmark_active.png'), inActiveIcon: require('../icons/bookmark.png'), component: SavedScreen },
     { route: 'Settings', label: 'Settings', activeIcon: require('../icons/settings_active.png'), inActiveIcon: require('../icons/settings.png'), component: SettingsScreen },
 ];
