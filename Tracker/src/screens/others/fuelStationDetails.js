4d15df3ccf92922740fe34b2fe929969a9624a1f
Sheryf Habib <sheryf.534@gmail.com>
2023-06-24 07:58:30 +0100

    header complete

--- Tracker/src/screens/others/fuelStationDetails.js
+++ Tracker/src/screens/others/fuelStationDetails.js
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Image, ScrollView, Dimensions, TouchableOpacity, TextInput } from 'react-native';
 import React from 'react'
 
+const { height, width } = Dimensions.get('window');
+
 const FuelStationDetails = () => {
-  return (
-    <View>
-      <Text>FuelStationDetails</Text>
-    </View>
-  )
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.backgroundImage}>
+                <ImageBackground source={require('../../images/backgrnd.png')} style={styles.fullBackground}>
+                    <View style={styles.header}>
+                        <TouchableOpacity>
+                            <Image style={{ width: 32, height: 30}} source={require('../../icons/back.png')}/>  
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image style={{ width: 30, height: 30 }} source={require('../../icons/options.png')} />
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
+            </View>
+        </ScrollView>
+    );
 }
 
 export default FuelStationDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'flex-start',
+        minHeight: height * 1.1
+    },
+    fullBackground: {
+        width: width,
+        height: height * 0.25,
+        objectFit: 'cover',
+        paddingHorizontal: '4%'
+    },
+    header: {
+        width: "100%",
+        height: 40,
+        top: 30,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+})
+
+
+
+
+
+
+
+
+
+
