2cd7466c4a54142e0f711fae7cf4eb041894ec57
Sheryf Habib <sheryf.534@gmail.com>
2023-06-27 09:00:44 +0000

    merge error

--- Tracker/src/screens/others/fuelStationDetails.js
+++ Tracker/src/screens/others/fuelStationDetails.js
@@ -230,7 +230,7 @@ const FuelStationDetails = ({ navigation }) => {
             >
                 <View style={styles.bottomSheetContent}>
                     <View style={styles.buttomsheetheader}>
-                        <
+                        <Text>Update Price</Text>
                         <TouchableOpacity onPress={closePriceOptionButton}>
                             <Image style={{ width: 30, height: 30 }} source={require('../../images/Icons.png')} />
                         </TouchableOpacity>
