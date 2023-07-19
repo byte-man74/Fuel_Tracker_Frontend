4f46fe535a3d42e924cb9ad9d4a2b58cc45442b6
Sheryf Habib <sheryf.534@gmail.com>
2023-07-19 10:40:46 +0100

    staging axios interceptor

---
+++ Tracker/src/services/api.js
@@ -0,0 +1,28 @@
+// api.js
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://pricewiz.pythonanywhere.com/api', // Replace with your API base URL
+});
+
+// Request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    // Check if a login token exists in AsyncStorage or any other storage mechanism of your choice
+    const userToken = await AsyncStorage.getItem('userAccessToken');
+
+    if (userToken) {
+      // If a token exists, add it as a header to the API request
+      config.headers['Authorization'] = `Bearer ${userToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
+export default api;
