ccc108dfed3976b38e79dbb14cbc60bc8de576b1
Sheryf Habib <sheryf.534@gmail.com>
2023-07-25 15:44:49 +0100

    navigation bug

--- Tracker/App.js
+++ Tracker/App.js
@@ -1,21 +1,21 @@
-import OnboardingNavigator from './src/navigations/OnboardingNavigator';
-import { NavigationContainer } from '@react-navigation/native';
-import AuthNavigator from './src/navigations/AuthenticationNavigator';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useCallback, useEffect, useState } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo';
-import MainScreenTab from './src/navigations/MainScreen';
-import SearchScreen from './src/screens/MainScreen/dashboard/searchScreen';
-import FuelStationDetails from './src/screens/others/fuelStationDetails';
-import StationList from './src/screens/others/stationList';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import NotificationScreen from './src/screens/others/notification';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Permission from './src/screens/OnboardingScreen/permission';
+import OnboardingNavigator from "./src/navigations/OnboardingNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./src/navigations/AuthenticationNavigator";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useCallback, useEffect, useState } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo";
+import MainScreenTab from "./src/navigations/MainScreen";
+import SearchScreen from "./src/screens/MainScreen/dashboard/searchScreen";
+import FuelStationDetails from "./src/screens/others/fuelStationDetails";
+import StationList from "./src/screens/others/stationList";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import NotificationScreen from "./src/screens/others/notification";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Permission from "./src/screens/OnboardingScreen/permission";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import NoNetworkScreen from './src/screens/others/noNetwork';
-import ServerScreen from './src/screens/others/serverError';
+import NoNetworkScreen from "./src/screens/others/noNetwork";
+import ServerScreen from "./src/screens/others/serverError";
 
 const Stack = createStackNavigator();
 
@@ -24,19 +24,18 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-
       // Check if token exists in AsyncStorage
-      const userToken = await AsyncStorage.getItem('userAccessToken');
+      const userToken = await AsyncStorage.getItem("userAccessToken");
       if (userToken) {
         setToken(userToken);
       }
     })();
   }, []);
   const [fontsLoaded] = useFonts({
-    "CustomFontRegular": require('./assets/fonts/Mulish-Black.ttf'),
-    "MulishBold": require('./assets/fonts/Mulish-Bold.ttf'),
-    "Regular": require('./assets/fonts/Mulish-Regular.ttf'),
-    "SemiBold": require('./assets/fonts/Mulish-SemiBold.ttf')
+    CustomFontRegular: require("./assets/fonts/Mulish-Black.ttf"),
+    MulishBold: require("./assets/fonts/Mulish-Bold.ttf"),
+    Regular: require("./assets/fonts/Mulish-Regular.ttf"),
+    SemiBold: require("./assets/fonts/Mulish-SemiBold.ttf"),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -49,35 +48,38 @@ export default function App() {
     return null;
   }
 
-
   return (
     <NavigationContainer>
       <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-      <Stack.Navigator screenOptions={{ headerShown: false }}> 
-        {token ? (
+        <BottomSheetModalProvider>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {token ? (
               // If token exists, navigate to MainScreen
-              <>
-                <Stack.Screen name="MainScreen" component={MainScreenTab} />
-                <Stack.Screen name="FuelStationDetails" component={FuelStationDetails} />
-                <Stack.Screen name="SearchScreen" component={SearchScreen} />
-                <Stack.Screen name="StationList" component={StationList} />
-                <Stack.Screen name="Notification" component={NotificationScreen} />
-              </>
+              <></>
             ) : (
               // If token doesn't exist, navigate to Onboarding
               <>
-                <Stack.Screen name="Authentication" component={AuthNavigator} />
-                <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
-                <Stack.Screen name="Permissions" component={Permission} />
+                <Stack.Screen
+                  name="Onboarding"
+                  component={OnboardingNavigator}
+                />
               </>
-            )} 
-              <Stack.Screen name="ServerScreen" component={ServerScreen} />
-              <Stack.Screen name="NoNetwork" component={NoNetworkScreen} />
-        </Stack.Navigator>
-      </BottomSheetModalProvider>
+            )}
+            <Stack.Screen name="MainScreen" component={MainScreenTab} />
+            <Stack.Screen
+              name="FuelStationDetails"
+              component={FuelStationDetails}
+            />
+            <Stack.Screen name="SearchScreen" component={SearchScreen} />
+            <Stack.Screen name="StationList" component={StationList} />
+            <Stack.Screen name="Notification" component={NotificationScreen} />
+            <Stack.Screen name="Authentication" component={AuthNavigator} />
+            <Stack.Screen name="ServerScreen" component={ServerScreen} />
+            <Stack.Screen name="Permissions" component={Permission} />
+            <Stack.Screen name="NoNetwork" component={NoNetworkScreen} />
+          </Stack.Navigator>
+        </BottomSheetModalProvider>
       </GestureHandlerRootView>
     </NavigationContainer>
   );
 }
-
