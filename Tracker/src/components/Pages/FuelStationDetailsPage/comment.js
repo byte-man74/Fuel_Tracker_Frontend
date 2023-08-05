fa210dfefc0e132b66e608885d85ebca3b5ae917
Sheryf Habib <sheryf.534@gmail.com>
2023-08-05 08:42:00 +0100

    refactored comments

--- Tracker/src/components/Pages/FuelStationDetailsPage/comment.js
+++ Tracker/src/components/Pages/FuelStationDetailsPage/comment.js
@@ -1,242 +1,74 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
-const CommentItem = ({ date, comment, name}) => {
-  console.log(comment)
+
+/**
+ * A component that displays a user comment with author information.
+ * 
+ * @param {string} date - The date of the comment.
+ * @param {string} comment - The content of the comment.
+ * @param {string} name - The name of the comment author.
+ */
+
+const CommentItem = ({ date, comment, name }) => {
   return (
-    <View style={styles.commentItem}>
-      <View style={styles.carouselContainerExtraInfo}>
+    <View style={styles.commentContainer}>
+      <View style={styles.authorInfo}>
         <Image
           source={require('../../../images/avatar.png')}
-          style={{
-            width: 35,
-            height: 35,
-            borderRadius: 400,
-            marginRight: 7,
-          }}
+          style={styles.avatar}
         />
-        <View style={styles.carouselContainerExtraInfoText}>
-          <Text style={styles.stationText}>{name}</Text>
-          <Text style={styles.stationLocation}>{date}</Text>
+        <View style={styles.authorDetails}>
+          <Text style={styles.authorName}>{name}</Text>
+          <Text style={styles.commentDate}>{date}</Text>
         </View>
       </View>
-      <Text style={styles.Text}>{comment}</Text>
+      <Text style={styles.commentText}>{comment}</Text>
     </View>
   );
 };
 
-export default CommentItem
-
+export default CommentItem;
 
 const styles = StyleSheet.create({
-    header: {
-      width: "100%",
-      height: 40,
-      top: 30,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    stationInfo: {
-      width: "100%",
-      height: 110,
-      paddingHorizontal: "4%",
-      flexDirection: "row",
-    },
-    searchContainer: {
-      width: "100%",
-      height: 50,
-      marginTop: 30,
-      marginBottom: 30,
-    },
-    searchInput: {
-      width: "100%",
-      height: "100%",
-      borderStyle: "solid",
-      borderWidth: 1,
-      borderColor: "#ABABAB",
-      borderRadius: 8,
-      paddingLeft: "2.2%",
-      fontFamily: "Regular",
-      fontSize: 16,
-      color: "#232323",
-    },
-    stationImage: {
-      width: 180,
-      height: 130,
-      marginTop: -30,
-      borderRadius: 10,
-      borderWidth: 5,
-      borderColor: "#D1D1D147",
-      marginRight: 7.5,
-    },
-    stationDetailsTextContainer: {
-      width: "48.5%",
-      height: "87%",
-      alignSelf: "center",
-      flexDirection: "row",
-    },
-    Text: {
-      fontFamily: "Regular",
-      fontSize: 16,
-      color: "#232323",
-    },
-    EditText: {
-      fontFamily: "MulishBold",
-      fontSize: 18,
-      color: "#232323",
-      marginRight: 10,
-    },
-    Text_link: {
-      fontFamily: "Regular",
-      fontSize: 16,
-      color: "#232323",
-      textDecorationLine: "underline",
-    },
-    TextBold: {
-      fontFamily: "SemiBold",
-      fontSize: 18,
-      color: "#232323",
-    },
-    TimeText: {
-      fontFamily: "Regular",
-      fontSize: 16,
-      color: "#232323",
-      position: "absolute",
-      right: 0,
-    },
-    extraFunctionsStyling: {
-      width: "90%",
-      height: 35,
-      flexDirection: "row",
-      alignItems: "center",
-      marginTop: 10,
-      justifyContent: "space-between",
-    },
-    upvoteButton: {
-      width: 150,
-      height: "100%",
-      backgroundColor: "#E8E9EE",
-      borderRadius: 8,
-      paddingHorizontal: 4,
-      alignItems: "center",
-      flexDirection: "row",
-      marginRight: 8,
-    },
-    dirButton: {
-      width: 108,
-      height: "100%",
-      backgroundColor: "#333333",
-      borderRadius: 8,
-      paddingHorizontal: 7,
-      alignItems: "center",
-      flexDirection: "row",
-      marginRight: 8,
-    },
-    openingHours: {
-      width: 157,
-      height: "100%",
-      backgroundColor: "#333333",
-      borderRadius: 8,
-      paddingHorizontal: 4,
-      alignItems: "center",
-      flexDirection: "row",
-      marginRight: 8,
-    },
-    trafficIndicator: {
-      width: 85,
-      height: "100%",
-      backgroundColor: "rgba(102, 189, 112, 1)",
-      borderRadius: 8,
-      paddingHorizontal: 4,
-      alignItems: "center",
-      flexDirection: "row",
-      marginRight: 8,
-    },
-    otherDetails: {
-      width: "94%",
-      minHeight: 120,
-      borderBottomColor: "#D9D9D9",
-      borderBottomWidth: 1,
-      marginBottom: 20,
-    },
-    secondDetails: {
-      width: "94%",
-      minHeight: 90,
-      borderBottomColor: "#D9D9D9",
-      borderBottomWidth: 1,
-      marginBottom: 20,
-      justifyContent: "space-around",
-    },
-    carouselContainerExtraInfo: {
-      width: "100%",
-      top: 0,
-      minHeight: 60,
-      flexDirection: "row",
-      alignItems: "center",
-    },
-    carouselContainerExtraInfoText: {
-      minWidth: 50,
-      minHeight: 40,
-      flexDirection: "column",
-      justifyContent: "space-around",
-    },
-    stationText: {
-      fontFamily: "SemiBold",
-      fontSize: 15,
-      color: "#232323",
-    },
-   stationLocation : {
-      fontFamily: "Regular",
-      fontSize: 14,
-      color: "#232323",
-    },
-    bookmarkIconStyling: {
-      width: 22,
-      height: 22,
-    },
-    commentItem: {
-      width: "100%",
-      minHeight: 80,
-      paddingBottom: 20,
-      borderBottomWidth: 2,
-      borderColor: "#D1D1D147",
-      marginBottom: 10,
-    },
-    bottomSheetContent: {
-      padding: 5,
-      alignItems: "center",
-    },
-    buttomsheetheader: {
-      width: "100%",
-      height: 50,
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      alignItems: "center",
-    },
-    buttomsheetheader2: {
-      width: "100%",
-      height: 50,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    feedbackContainer: {
-      width: "100%",
-      minHeight: 70,
-      marginTop: 20,
-      alignItems: "center",
-    },
-    radioContainer: {
-      flexDirection: "row", // Place radio options horizontally
-      alignItems: "center", // Align items vertically within the container
-      paddingLeft: 2,
-      marginRight: 6, // Adjust the spacing between radio options as needed
-    },
-    radioOption: {
-      flexDirection: "row", // Align radio button and text horizontally
-      alignItems: "center", // Align items vertically within each option
-      marginRight: 10, // Adjust the spacing between radio options as needed
-    },
-  });
-  
+  commentContainer: {
+    width: '100%',
+    minHeight: 80,
+    paddingBottom: 20,
+    borderBottomWidth: 2,
+    borderColor: '#D1D1D147',
+    marginBottom: 10,
+  },
+  authorInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 60,
+  },
+  avatar: {
+    width: 35,
+    height: 35,
+    borderRadius: 400,
+    marginRight: 7,
+  },
+  authorDetails: {
+    minWidth: 50,
+    minHeight: 40,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  authorName: {
+    fontFamily: 'SemiBold',
+    fontSize: 15,
+    color: '#232323',
+  },
+  commentDate: {
+    fontFamily: 'Regular',
+    fontSize: 14,
+    color: '#232323',
+  },
+  commentText: {
+    fontFamily: 'Regular',
+    fontSize: 16,
+    color: '#232323',
+  },
+});
