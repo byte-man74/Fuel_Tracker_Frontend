48094b33d2511e4cd8f2160be5302e2d07d7c857
Sheryf Habib <sheryf.534@gmail.com>
2023-10-19 09:23:52 +0100

    final clean up

--- Tracker/src/screens/MainScreen/dashboard/home/components/averagePrice.js
+++ Tracker/src/screens/MainScreen/dashboard/home/components/averagePrice.js
@@ -1,18 +1,54 @@
-import { View, Text, Image } from 'react-native';
-import { styles } from '../styles/homeStyles';
+import { View, Text, Image } from "react-native";
+import { styles } from "../styles/homeStyles";
+import { useState, useEffect } from "react";
+import api from "../../../../../services/api";
+import { ActivityIndicator } from "react-native";
 
-export const AveragePrice = () => (
+export const AveragePrice = () => {
+  const [price, setPrice] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const get_avg_price = async () => {
+      try {
+        const response = await api.get("get_average_price");
+        console.log(response.data);
+        if (response.status === 200) {
+          
+          setPrice(response.data.avg_amount.toFixed(1));
+        } else {
+          console.error("Error: Unexpected response status:", response.status);
+        }
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    get_avg_price();
+  }, []);
+
+  return (
     <View style={styles.averagePriceBox}>
       <View style={styles.averagePriceBoxContainer}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={styles.priceText}>₦---</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          {isLoading ? (
+            <ActivityIndicator size="small" color="rgba(245, 168, 85, 1)" />
+          ) : (
+            <Text style={styles.priceText}>
+              ₦{price == null ? "---" : price}
+            </Text>
+          )}
           <Image
             style={{ width: 24, height: 24, marginLeft: 2 }}
-            source={require('../../../../../icons/gas.png')}
+            source={require("../../../../../icons/gas.png")}
           />
         </View>
         <Text style={styles.headerText}>Average fuel price near you</Text>
       </View>
     </View>
   );
-  
+};
